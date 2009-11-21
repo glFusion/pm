80488cf94fdc3f908beecfa3e55ff18b9c92a3a7
Mark R. Evans <mark@glfusion.org>
2009-11-21 21:27:37 +0000

    New Dutch and Swedish translations.

---
+++ language/dutch_utf-8.php
@@ -0,0 +1,152 @@
+<?php
+// +--------------------------------------------------------------------------+
+// | PM Plugin - glFusion CMS                                                 |
+// +--------------------------------------------------------------------------+
+// | dutch.php                                                              |
+// |                                                                          |
+// | Dutch language file                                                    |
+// +--------------------------------------------------------------------------+
+// | $Id::                                                                   $|
+// +--------------------------------------------------------------------------+
+// | Copyright (C) 2009 by the following authors:                             |
+// |                                                                          |
+// | Mark R. Evans          mark AT glfusion DOT org                          |
+// +--------------------------------------------------------------------------+
+// |                                                                          |
+// | This program is free software; you can redistribute it and/or            |
+// | modify it under the terms of the GNU General Public License              |
+// | as published by the Free Software Foundation; either version 2           |
+// | of the License, or (at your option) any later version.                   |
+// |                                                                          |
+// | This program is distributed in the hope that it will be useful,          |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
+// | GNU General Public License for more details.                             |
+// |                                                                          |
+// | You should have received a copy of the GNU General Public License        |
+// | along with this program; if not, write to the Free Software Foundation,  |
+// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.          |
+// |                                                                          |
+// +--------------------------------------------------------------------------+
+
+if (!defined ('GVERSION')) {
+    die ('This file can not be used on its own.');
+}
+
+$LANG_PM00 = array (
+    'menulabel'         => 'Prive Berichten',
+    'plugin'            => 'pm',
+    'admin_menu'        => 'PM Configuratie',
+    'pm_index'          => 'PM Index',
+    'pm'                => 'PM',
+    'user_menu'         => 'Berichten Center',
+    'title'             => 'Prive Bericht',
+    'compose_msg'       => 'Schrijf een Bericht',
+    'reply_msg'         => 'Beantwoord Bericht',
+    'preview_msg'       => 'Bekijk Bericht',
+    'quote_msg'         => 'Quote Bericht',
+    'delete_msg'        => 'Verwijder Bericht',
+    'inbox'             => 'Inbox',
+    'sent'              => 'Sent',
+    'archive'           => 'Archief',
+    'outbox'            => 'Outbox',
+    'all_messages'      => 'Alle Berichten',
+    'one_day'           => '1 Dag',
+    'seven_days'        => '7 Dagen',
+    'two_weeks'         => '2 Weken',
+    'one_month'         => '1 Maand',
+    'three_months'      => '3 Maanden',
+    'six_months'        => '6 Maanden',
+    'one_year'          => '1 Jaar',
+    'author'            => 'Auteur',
+    'post_time'         => 'Post Time',
+    'subject'           => 'Onderwerp',
+    'ascending'         => 'Oplopend',
+    'descending'        => 'Aflopend',
+    'no_subject'        => 'Geen onderwerp beschikbaar...',
+    'by'                => 'Door',
+    'to'                => 'Aan',
+    'folder'            => 'Folder',
+    'view_folder'       => 'Bekijk folder',
+    'go'                => 'Ga',
+    'messages'          => 'BERICHTEN',
+    'select'            => 'SELECTEER',
+    'display'           => 'Toon',
+    'sort_by'           => 'Sorteer Op',
+    'view_msg'          => 'Bekijk Bericht Van',
+    'return_to'         => 'Return To',
+    'sent'              => 'Verzonden',
+    'from'              => 'Van',
+    'registered'        => 'Geregistreerd op',
+    'location'          => 'Location',
+    'homepage'          => 'Homepage',
+    'move_to_folder'    => 'Verplaats naar Folder',
+    'mark_all'          => 'Mark All',
+    'unmark_all'        => 'Unmark All',
+    'delete_marked'     => 'Deleted Marked',
+    'archive_marked'    => 'Move marked to Saved messages',
+    'message_history'   => 'Berichten Historie',
+    'add_user'          => 'Voeg een Gebruiker toe',
+    'add_friend'        => 'Voeg een Vriend(in) toe',
+    'send'              => 'Verstuur',
+    'preview'           => 'Bekijk',
+    'cancel'            => 'Annuleer',
+    'submit'            => 'Uitvoeren',
+    'batch_confirm'     => 'Are you sure you want to move or delete the selected messages?',
+    'delete_confirm'    => 'Are you sure you want to delete this message?',
+    'no_messages'       => 'Er zijn geen berichten in deze folder',
+    'manage_friends'    => 'Beheer uw Vrienden',
+    'friend_help'       => 'Friends enable you quick access to members you communicate with frequently.',
+    'your_friends'      => 'Uw vrienden',
+    'your_friends_help' => 'To remove usernames select them and click submit.',
+    'add_new_friend'    => 'Voeg Nieuwe Vrienden toe',
+    'add_new_friend_help' => 'You may enter several usernames separated by a comma.',
+    'in_friends_list'   => 'Is Uw Vriend(in)',
+    'send_pm'           => 'Verstuur een Prive Bericht',
+);
+
+$LANG_PM_NOTIFY = array(
+    'pm_notify'         => 'PM Meldingen',    
+    'new_pm_notification' => 'Melding Nieuw Prive Bericht',
+    'hello'             => 'Hallo',
+    'subject'           => 'Onderwerp',
+    'new_pm_text'       => 'U heeft een nieuw Prive bericht van',
+    'disclaimer'        => 'You are receiving this because you allow email from the site admin.  To change your privacy settings, please use the My Account setting at ' . $_CONF['site_url'] ,
+    'sincerely'         => 'Bedankt!',
+    'support'           => 'Support',
+);
+
+$LANG_PM_ERROR = array(
+    'token_failure'     => 'Security Token Failure',
+    'message_not_found' => 'Bericht niet gevonden',
+    'no_to_address'     => 'No to address specified',
+    'no_subject'        => 'Subject must not be blank and must be greater than 4 characters in length.',
+    'no_message'        => 'Message body must not be blank and must be greater than 4 characters in length.',
+    'unknown_user'      => 'Kan de volgende gebruiker niet vinden:',
+    'too_many_recipients' => 'You have included too many recipients - Maximum %s allowed.',
+);
+
+// Localization of the Admin Configuration UI
+$LANG_configsections['pm'] = array(
+    'label'                 => 'Prive Berichten',
+    'title'                 => 'Configuratie Prive Berichten',
+);
+$LANG_confignames['pm'] = array(
+    'messages_per_page'     => 'Berichten per pagina',
+    'post_speedlimit'       => 'Posting Speedlimit (seconds)',
+    'max_recipients'        => 'Max aantal ontvangers per bericht',
+);
+$LANG_configsubgroups['pm'] = array(
+    'sg_main'               => 'Hoofd Instellingen',
+);
+
+$LANG_fs['pm'] = array(
+    'pm_general'            => 'PM Algemene Instellingen',
+);
+
+$PLG_pm_MESSAGE1 = 'Message(s) successfully sent.';
+$PLG_pm_MESSAGE2 = 'Message(s) successfully deleted.';
+$PLG_pm_MESSAGE3 = 'Message(s) successfully archived.';
+$PLG_pm_MESSAGE4 = 'Private Message Speedlimit Hit - Please wait a minute before sending another message.';
+$PLG_pm_MESSAGE5 = 'Friends List Updated.';
+?>
---
+++ language/swedish_utf-8.php
@@ -0,0 +1,156 @@
+<?php
+// +--------------------------------------------------------------------------+
+// | PM Plugin - glFusion CMS                                                 |
+// +--------------------------------------------------------------------------+
+// | swedish_utf-8.php                                                        |
+// |                                                                          |
+// | Swedish language file                                                    |
+// +--------------------------------------------------------------------------+
+// | $Id::                                                                   $|
+// +--------------------------------------------------------------------------+
+// | Copyright (C) 2009 by the following authors:                             |
+// |                                                                          |
+// | Mark R. Evans          mark AT glfusion DOT org                          |
+// +--------------------------------------------------------------------------+
+// |                                                                          |
+// | This program is free software; you can redistribute it and/or            |
+// | modify it under the terms of the GNU General Public License              |
+// | as published by the Free Software Foundation; either version 2           |
+// | of the License, or (at your option) any later version.                   |
+// |                                                                          |
+// | This program is distributed in the hope that it will be useful,          |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
+// | GNU General Public License for more details.                             |
+// |                                                                          |
+// | You should have received a copy of the GNU General Public License        |
+// | along with this program; if not, write to the Free Software Foundation,  |
+// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.          |
+// |                                                                          |
+// +--------------------------------------------------------------------------+
+
+if (!defined ('GVERSION')) {
+    die ('This file can not be used on its own.');
+}
+
+$LANG_PM00 = array (
+    'menulabel'         => 'Privata Meddelanden',
+    'plugin'            => 'pm',
+    'admin_menu'        => 'PM Admin',
+    'pm_index'          => 'PM Index',
+    'pm'                => 'PM',
+    'user_menu'         => 'Meddelande Center',
+    'title'             => 'Privata Meddelanden',
+    'compose_msg'       => 'Skriv meddelande',
+    'reply_msg'         => 'Svara meddelande',
+    'preview_msg'       => 'Förhandsvisa meddelande',
+    'quote_msg'         => 'Citera  meddelande',
+    'delete_msg'        => 'Radera meddelande',
+    'inbox'             => 'Inkorg',
+    'sent'              => 'Skickade',
+    'archive'           => 'Arkiv',
+    'outbox'            => 'Utbox',
+    'all_messages'      => 'Alla meddelanden',
+    'one_day'           => '1 dag',
+    'seven_days'        => '7 dagar',
+    'two_weeks'         => '2 veckor',
+    'one_month'         => '1 månad',
+    'three_months'      => '3 månader',
+    'six_months'        => '6 månader',
+    'one_year'          => '1 år',
+    'author'            => 'skribent',
+    'post_time'         => 'Post-tid',
+    'subject'           => 'Ämne',
+    'ascending'         => 'Stigandes',
+    'descending'        => 'Fallande',
+    'no_subject'        => 'Inget ämne tillgängligt...',
+    'by'                => 'Av',
+    'to'                => 'Till',
+    'folder'            => 'Katalog',
+    'view_folder'       => 'Visa katalog',
+    'go'                => 'Gå',
+    'messages'          => 'MEDDELANDEN',
+    'select'            => 'VÄLJ',
+    'display'           => 'Visa',
+    'sort_by'           => 'Sortera efter',
+    'view_msg'          => 'Vissa meddelande från',
+    'return_to'         => 'Svara till',
+    'sent'              => 'Skickat',
+    'from'              => 'Från',
+    'registered'        => 'Blev medlem',
+    'location'          => 'Plats',
+    'homepage'          => 'Hemsida',
+    'move_to_folder'    => 'Flytta till katalog',
+    'mark_all'          => 'Markera alla',
+    'unmark_all'        => 'Avmarkera alla',
+    'delete_marked'     => 'Radera markerade',
+    'archive_marked'    => 'Flytta markerade till Sparade meddelanden',
+    'message_history'   => 'Meddelandehistorik',
+    'add_user'          => 'Lägg till användare',
+    'add_friend'        => 'Lägg till vän',
+    'send'              => 'Skicka',
+    'preview'           => 'Förhandsvisa',
+    'cancel'            => 'Avbryt',
+    'submit'            => 'Skicka',
+    'batch_confirm'     => 'Är du säker på att du vill flytta eller radera de valda meddelandena',
+    'delete_confirm'    => 'Är du säker på att du vill radera detta meddelande?',
+    'no_messages'       => 'Det finns inga meddelande i denna katalog',
+    'manage_friends'    => 'Hantera vänner',
+    'friend_help'       => 'Vänner gör så att du snabbt får tillgång till medlemmar som du skriver till ofta.',
+    'your_friends'      => 'Din vännerlista',
+    'your_friends_help' => 'För att avlägsna användarnamn välj dem och klicka på skicka.',
+    'add_new_friend'    => 'Lägg till ny vän',
+    'add_new_friend_help' => 'Du kan lägga till flera användare genom att separera dessa med ett kommatecken.',
+    'in_friends_list'   => 'Är din vän',
+    'send_pm'           => 'Skicka privat meddelande',
+    'error'             => ' Error',
+    'newpm'             => '<b>Du har %d nytt/nya </b><a href="'.$_CONF['site_url'].'/pm/index.php"><b>privata meddelande(n)</b>',
+);
+
+$LANG_PM_NOTIFY = array(
+    'pm_notify'         => 'PM besked',
+    'new_pm_notification' => 'Nytt privat meddelande-besked',
+    'hello'             => 'Tjenare',
+    'subject'           => 'Ämne',
+    'new_pm_text'       => 'Du har ett nytt privat meddelande från',
+    'disclaimer'        => 'Du har mottagit detta för du tillåter "privat meddelande-besked". För att ändra dina sekretessinställningar, var god använd Mitt konto-alternativ på ' . $_CONF['site_url'] ,
+    'sincerely'         => 'Tackar!',
+    'support'           => 'Support',
+);
+
+$LANG_PM_ERROR = array(
+    'token_failure'     => 'Felande säkerhetsnyckel',  
+    'message_not_found' => 'Meddelande ej funnet',
+    'no_to_address'     => 'Ingen till adress specifierad',
+    'no_subject'        => 'Ämnet får ej vara blankt och måste vara längre än 4 karaktärer.',
+    'no_message'        => 'Meddelandet får inte vara blankt och måste minst vara 4 karaktärer långt.',
+    'unknown_user'      => 'Kunde inte hitta användare:',
+    'too_many_recipients' => 'Du har lagt till för många mottagare - Max %s tillåtna.',
+    'invalid_msg_id'    => 'Invalid meddelande-ID',
+    'invalid_reply_id'  => 'Invalid svars-ID',
+);
+
+// Localization of the Admin Configuration UI
+$LANG_configsections['pm'] = array(
+    'label'                 => 'Privata Meddelanden',
+    'title'                 => 'Privata Meddelanden konfiguration'
+);
+$LANG_confignames['pm'] = array(
+    'messages_per_page'     => 'Meddelanden per sida',
+    'post_speedlimit'       => 'Postnings hastighetsgräns (sekunder)',
+    'max_recipients'        => 'Max antal mottagare per meddelande',
+);
+$LANG_configsubgroups['pm'] = array(
+    'sg_main'               => 'Huvudinställningar',
+);
+
+$LANG_fs['pm'] = array(
+    'pm_general'            => 'PM Allmänna inställningar',
+);
+
+$PLG_pm_MESSAGE1 = 'Meddelande(n) skickat.';
+$PLG_pm_MESSAGE2 = 'Meddelande(n) raderad.';
+$PLG_pm_MESSAGE3 = 'Meddelande(n) sparades.';
+$PLG_pm_MESSAGE4 = 'Privata Meddelandes fartbegränsning nådd - Var god vänta några menuter innan du skickar ännu ett meddelande.';
+$PLG_pm_MESSAGE5 = 'Vännerlistan uppdaterad.';
+?>
