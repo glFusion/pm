a2659c4170fdbf2269f2689c002e34782f106adf
Mark R. Evans <mark@glfusion.org>
2010-04-16 14:31:21 +0000

    Bump glFusion version to 1.2.0

--- pm.php
+++ pm.php
@@ -34,7 +34,7 @@ if (!defined ('GVERSION')) {
 $_PM_CONF['pi_name']           = 'pm';
 $_PM_CONF['pi_display_name']   = 'Private message';
 $_PM_CONF['pi_version']        = '1.2.0';
-$_PM_CONF['gl_version']        = '1.1.8';
+$_PM_CONF['gl_version']        = '1.2.0';
 $_PM_CONF['pi_url']            = 'http://www.glfusion.org';
 
 $_PM_table_prefix = $_DB_table_prefix . 'pm_';
